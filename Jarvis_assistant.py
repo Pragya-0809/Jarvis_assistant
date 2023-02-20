73ba800e109dff0d3b5b6e1c5f50fb0290acadc6
Pragya-0809 <pragyammpabf@gmail.com>
2023-02-20 19:56:57 +0530

    first commit

---
+++ Jarvis_assistant.py
@@ -0,0 +1,71 @@
+import pyttsx3                     #text-to-speech conversion library
+import speech_recognition as sr    #Library for performing speech recognition
+import webbrowser                  #displaying web-based documents to users
+import datetime                    #supplies classes for manipulating dates and times.
+import pyjokes                     #used to create one-line jokes for programmers
+import time
+
+#function to convert speech to text
+def sptext():
+    recognizer=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening......")
+        recognizer.adjust_for_ambient_noise(source)           #cancelling noise
+        audio=recognizer.listen(source)                       #it will listen to the source
+        try:
+            print("Recognizing.....")
+            data=recognizer.recognize_google(audio)
+            print(data)
+            return data
+        except sr.UnknownValueError:
+            print("Not Understanding")
+
+def txtsp(x):
+    engine=pyttsx3.init()
+    voices=engine.getProperty('voices')
+    engine.setProperty('voice',voices[0].id)         #0->male, 1->female
+    rate=engine.getProperty('rate')
+    engine.setProperty('rate',150)
+    engine.say(x)
+    engine.runAndWait()
+    
+#splitting the function
+if __name__=='__main__':
+
+        
+    if "peter" in sptext().lower():    #activating the system
+        while True:
+            data1=sptext().lower()
+            
+            if "your name" in data1:
+                name="my name is peter"
+                txtsp(name)
+
+            elif "old are you" in data1:
+                txtsp("I am two years old")
+
+            elif "now time" in data1:
+                time=datetime.datetime.now().strftime("%I%M%p")     #%M->minute, %p->AM/PM, %I->hours
+                txtsp(time)
+
+            elif 'youtube' in data1:
+                webbrowser.open("https://www.youtube.com/")
+
+            elif 'news' in data1:
+                webbrowser.open("https://www.google.com/search?q=latest+news&rlz=1C1CHWL_enIN991IN991&sxsrf=AJOqlzU9idgROmrxUz1WEq0akDaJnwfoCg%3A1676630932954&ei=lFvvY_HxObG73LUP_-e8oAk&ved=0ahUKEwjxi7b5sJz9AhWxHbcAHf8zD5QQ4dUDCA8&uact=5&oq=latest+news&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIECCMQJzIKCAAQsQMQgwEQQzIKCAAQsQMQgwEQQzIECAAQQzIKCAAQsQMQgwEQQzIECAAQQzIECAAQQzIECAAQQzITCAAQgAQQFBCHAhCxAxCDARDJAzINCAAQgAQQFBCHAhCxAzoKCAAQRxDWBBCwAzoICAAQkgMQsAM6CgguELEDEIMBEEM6BQgAEIAEOgsIABCABBCxAxCDAToHCAAQsQMQQzoHCAAQgAQQCjoHCC4QgAQQCkoECEEYAFC9BFjhJWDgJ2gCcAF4AIABogaIAa4XkgEFNS0xLjOYAQCgAQHIAQrAAQE&sclient=gws-wiz-serp")
+
+            elif 'joke' in data1:
+                joke_1=pyjokes.get_joke(language="en", category="neutral")
+                print(joke_1)
+                txtsp(joke_1)
+
+            elif 'play song' in data1:
+                webbrowser.open("https://www.youtube.com/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ")
+
+            elif "exit" in data1:
+                txtsp("Thank You")
+                break
+            else:
+                print("Thanks")
+                
+            time.sleep(5)
